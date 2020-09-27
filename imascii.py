5be51557e778f187b79e016a8fb455d49a885649
mornville <aj97389@gmail.com>
2020-09-27 12:45:40 +0530

    Added oops

--- imascii.py
+++ imascii.py
@@ -1,40 +1,48 @@
 from PIL import Image
 ASCII_CHARS = ['@', '#', '$', '%', '?', "*", "+", ";", ":", ',', '.']
 
-def resize_image(image, new_width):
-    width, height = image.size
-    ratio = height/width
-    new_height = int(new_width*ratio)
-    resize_image = image.resize((new_width, new_height))
+class imascii():
+    def __init__(self, path, format='txt', new_width=100):
+        self.path = path
+        self.format = format
+        self.new_width = new_width
 
-    return resize_image
+    def resize_image(self, image):
+        width, height = image.size
+        ratio = height/width
+        new_height = int(self.new_width*ratio)
+        resize_image = image.resize((self.new_width, new_height))
 
-def convet_to_gray_scale(image):
-    gray_image = image.convert("L")
-    return gray_image
+        return resize_image
 
-def pix_toAscii(image):
-    pixels = image.getdata()
-    characters = "".join([ASCII_CHARS[pixel//25] for pixel in pixels])
-    return characters
+    def convet_to_gray_scale(self, image):
+        gray_image = image.convert("L")
+        return gray_image
 
-def main():
-    path = input("Enter image path: ")
-    new_width = int(input("Enter required width: "))
-    try:
-        image = Image.open(path)
-    except Exception as e:
-        print(e)
-        return
+    def pix_toAscii(self, image):
+        pixels = image.getdata()
+        characters = "".join([ASCII_CHARS[pixel//25] for pixel in pixels])
+        return characters
 
-    new_image_data = pix_toAscii(convet_to_gray_scale(resize_image(image, new_width)))
+    def main(self):
+
+        try:
+            image = Image.open(self.path)
+        except Exception as e:
+            print(e)
+            return
+
+        new_image_data = self.pix_toAscii(self.convet_to_gray_scale(self.resize_image(image)))
 
 
-    pixel_count = len(new_image_data)
-    ascii_image = "\n".join(new_image_data[i:(i+new_width)] for i in range(0, pixel_count, new_width))
-    print(ascii_image)
-    with open("out.txt", "w") as f:
-        f.write(ascii_image)
+        pixel_count = len(new_image_data)
+        ascii_image = "\n".join(new_image_data[i:(i+self.new_width)] for i in range(0, pixel_count, self.new_width))
+        if self.format=='txt':
+            with open("out.txt", "w") as f:
+                f.write(ascii_image)
+        else:
+            print(ascii_image)
 
-    
-main()
+        
+a = imascii(path='pika.png',format='txt', new_width=100)
+a.main()
